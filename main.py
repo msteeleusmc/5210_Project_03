74fe28dbc654d29bdea285821d173956a5470680
Michael <michaelsteele@my.unt.edu>
2021-10-28 13:18:17 -0500

    added funciton to create vert/horiz lines on game board

--- main.py
+++ main.py
@@ -11,10 +11,24 @@ class TicTacToe():
         self.canvas = Canvas(self.window, width=700, height=700)
         self.canvas.pack()
 
+        # Game board needs to be initialized
+        self.build_board()
 
     # Mainloop function
     def mainloop(self):
         self.window.mainloop()
+
+    # Build board function
+    def build_board(self):
+        """Both for loops will create lines in the tkinter window
+        to separate game spaces when playing against the AI"""
+        # Creates two vertical lines
+        for index in range(2):
+            self.canvas.create_line((index + 1) * 700 / 3, 0, (index + 1) * 700 / 3, 700)
+        # Creates two horizontal lines
+        for index in range(2):
+            self.canvas.create_line(0, (index + 1) * 700 / 3, 700, (index + 1) * 700 / 3)
+
 
 if __name__ == '__main__':
     board = TicTacToe()
